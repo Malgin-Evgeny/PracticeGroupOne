beae80f68956e5f2b0e5a26dc10edebe62892635
Anton Piyadin <piyadinanton08080202@gmail.com>
2023-10-07 13:04:52 +0300

    create xpath for welcomePage

--- src/test/java/ui/pages/LoginPage.java
+++ src/test/java/ui/pages/LoginPage.java
@@ -11,23 +11,28 @@ import static com.codeborne.selenide.Selenide.*;
 public class LoginPage extends TestBase {
     private final SelenideElement loginInput = $x("//*[@id=\":r0:\"]");
     private final SelenideElement passwordInput = $x("//*[@id=\":r1:\"]");
-    private final SelenideElement loginButton = $x("//*[@class=\"MuiButtonBase-root MuiButton-root " +
-            "MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium " +
-            "MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium " +
-            "MuiButton-containedSizeMedium css-y4p3a9\"]");
+    private final SelenideElement loginButton = $x("//button[contains(@class, 'MuiButton-containedPrimary') or contains(text(), 'Войти')]");
     private final String login = "aqa_01";
     private final String password = "783891";
     private final SelenideElement welcome = $x("//*[@class=\"TitleBase_title-base__n1Oxe\"]");
+
     @Step("Вход в систему")
     public void loginVoid() {
         getUrl();
-        loginInput.shouldBe(Condition.visible);
-        actions().moveToElement(loginInput).click().sendKeys(login).perform();
-        actions().moveToElement(passwordInput).click().sendKeys(password).perform();
-        actions().moveToElement(loginButton).click().perform();
+        actions()
+                .moveToElement(loginInput)
+                .click().sendKeys(login)
+                .moveToElement(passwordInput)
+                .click()
+                .sendKeys(password)
+                .moveToElement(loginButton)
+                .click()
+                .perform();
     }
+
     @Step("Проверка Элемента")
     public void assertLoginSuccess() {
         welcome.shouldBe(Condition.visible);
         Assert.assertTrue(welcome.isDisplayed());
-}}
+    }
+}
