db816a5d32c109335da4d59d0a139dc5bcada1fa
Anton Piyadin <piyadinanton08080202@gmail.com>
2023-10-31 23:18:21 +0300

    create xpath dashboard

--- src/test/java/ui/baseItems/TestBase.java
+++ src/test/java/ui/baseItems/TestBase.java
@@ -65,4 +65,3 @@ public class TestBase {
         selenideElement.click();
     }
 }
-
--- src/test/java/ui/pages/DashBoardPanel.java
+++ src/test/java/ui/pages/DashBoardPanel.java
@@ -1,5 +1,14 @@
 package ui.pages;
 
-public class DashBoardPanel {
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$x;
+
+public class DashBoardPanel {
+    private final SelenideElement courses= $x("//*[contains(text(), 'Курсы')]");
+    private final SelenideElement users = $x("//*[contains(text(), 'Пользователи')]");
+    private final SelenideElement maximaPlay = $x("//*[contains(text(), 'Maxima Play')]");
+    private final SelenideElement more =$x("//*[contains(text(), 'Дополнительно')]");
+    private final SelenideElement brightnessForIcon=$x("//*[@data-testid='Brightness4Icon']");
+    private final SelenideElement settingsIcon = $x("//*[@data-testid='SettingsIcon']");
 }
